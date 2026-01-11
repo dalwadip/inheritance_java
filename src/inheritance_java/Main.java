7e5a257cdd92052aef053aad0cb5c841764ba722
P Dalwadi <dalwad63@rowan.edu>
2026-01-11 18:26:13 -0500

    Adding a file

--- src/inheritance_java/Main.java
+++ src/inheritance_java/Main.java
@@ -16,26 +16,31 @@ public class Main {
 		surgeon.operate();
 		surgeon.education();
 		System.out.println(surgeon.is_active);
+		System.out.println();
 		
 		System.out.println(dentist.hasMD);
 		dentist.oralCare();
 		dentist.education();
 		System.out.println(dentist.treats_gum);
+		System.out.println();
 		
 		Doctor doctor = new Doctor(true);
 		doctor.setName("Trina");
 		Doctor mysurgeon = new Surgeon(true, true); //polymorphism
 		mysurgeon.assist();
+		System.out.println();
 		
 		System.out.println(doctor.getName());
 		doctor.setId(9387565);
 		System.out.println(doctor.getId());
+		System.out.println();
 		
 		Horse horse = new Horse();
 		horse.eat();
 		horse.sound();
 		horse.reproduce();
 		horse.warm();
+		System.out.println();
 		
 		Doctor doc1 = new Doctor(true);
 		Doctor doc2 = new Doctor(false);
@@ -45,8 +50,19 @@ public class Main {
 		//make for loop that iterates through that list, then print out size of array/list
 		for(int i = 0; i < docs.length; i++) { // repeats if need to go through lots of arrays
 			System.out.println(docs[i]); //prints index 0 and 1
+			System.out.println(doc1.hasMD); // prints doc attribute in for loop
 		}
-		System.out.println(docs.length);
+		System.out.println(docs.length); // gets size of list outside of for loop
+		System.out.println();
+		
+		//make while loop that does same as for loop
+		int i = 0;
+		while(i < docs.length) {
+			System.out.println(docs[i]); // prints true for index 0 first, then goes to next line, repeats for index 1
+			System.out.println(doc1.hasMD); // prints true for all since doc1 is always true
+			i++;
+		}
+		System.out.println(docs.length); // has a length of 2: doc1, doc2
 	}
 
 }
